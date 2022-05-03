f50ad07f7396fbbcf97118cba44153876d5ad2aa
ioxbank <96214235+ioxbank@users.noreply.github.com>
2022-05-03 11:39:15 +0300

    Update FeePoolOperation.jsx

--- app/components/Account/FeePoolOperation.jsx
+++ app/components/Account/FeePoolOperation.jsx
@@ -344,7 +344,9 @@ class FeePoolOperation extends React.Component {
                   "short_backing_asset"
               ])
             : "1.3.0";
-        let backingAssetSymbol = backingAsset.get("symbol");
+
+        const core_asset_symbol = core.get("symbol");
+            
         let dynamicObject = getDynamicObject(
             asset.get("dynamic_asset_data_id")
         );
@@ -385,7 +387,7 @@ class FeePoolOperation extends React.Component {
                 <Translate
                     component="p"
                     content="explorer.asset.fee_pool.accumulated_collateral_fees"
-                    asset={backingAssetSymbol}
+                    asset={core_asset_symbol}
                 />
                 <div style={{paddingBottom: "1rem"}}>
                     <Translate content="explorer.asset.fee_pool.accumulated_collateral_fees" />
